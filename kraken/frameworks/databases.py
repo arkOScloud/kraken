6015eb9a29a88ba9b25a8baa446d346c6a115e8e
Jacob Cook <jacob@peakwinter.net>
2015-02-05 15:26:04 -0500

    fixes after testing to databases

--- kraken/frameworks/databases.py
+++ kraken/frameworks/databases.py
@@ -15,7 +15,7 @@ class DatabasesAPI(MethodView):
         if request.args.get("rescan", None):
             databases.scan()
         dbs = databases.get(id)
-        if not dbs:
+        if id and not dbs:
             abort(404)
         if id and request.args.get("download", None):
             result = base64.b64encode(dbs.dump())
@@ -27,21 +27,19 @@ class DatabasesAPI(MethodView):
             return jsonify(database=dbs.as_dict())
     
     def post(self):
-        data = json.loads(request.body)["database"]
+        data = json.loads(request.data)["database"]
         message = Message()
-        app = applications.get(data["type"])
-        db = app.modules["database"]["db"]
-        db = db(data["id"], data["name"], app.modules["database"]["manager"])
+        manager = databases.get_managers(data["type"])
         try:
-            db.add()
+            db = manager.add_db(data["name"])
             message.complete("success", "%s created successfully" % data["name"])
             return jsonify(database=db.as_dict())
         except Exception, e:
             message.complete("error", "%s could not be created: %s" % (data["name"], str(e)))
-            abort(500)
+            raise
     
     def put(self, id):
-        data = json.loads(request.body)["database"]
+        data = json.loads(request.data)["database"]
         db = databases.get(id)
         if not id or not db:
             abort(404)
@@ -55,15 +53,14 @@ class DatabasesAPI(MethodView):
         message = Message()
         db = databases.get(id)
         if not id or not db:
-            alert(404)
+            abort(404)
         try:
             db.remove()
             message.complete("success", "%s removed successfully" % db.name)
-            emit("clients/databases/remove", json.dumps({"website": {"id": db.id}}))
             return Response(status=204)
         except Exception, e:
             message.complete("error", "%s could not be removed: %s" % (db.name, str(e)))
-            abort(500)
+            raise
 
 
 class DatabaseUsersAPI(MethodView):
@@ -71,7 +68,7 @@ class DatabaseUsersAPI(MethodView):
         if request.args.get("rescan", None):
             databases.scan_users()
         u = databases.get_user(id)
-        if not u:
+        if id and not u:
             abort(404)
         if type(u) == list:
             return jsonify(database_users=[x.as_dict() for x in u])
@@ -79,7 +76,7 @@ class DatabaseUsersAPI(MethodView):
             return jsonify(database_user=u.as_dict())
     
     def post(self):
-        data = json.loads(request.body)["database_user"]
+        data = json.loads(request.data)["database_user"]
         message = Message()
         app = applications.get(data["type"])
         u = app.modules["database"]["user"]
@@ -90,10 +87,10 @@ class DatabaseUsersAPI(MethodView):
             return jsonify(database_user=u.as_dict())
         except Exception, e:
             message.complete("error", "%s could not be created: %s" % (u.name, str(e)))
-            abort(500)
+            raise
     
     def put(self, id):
-        data = json.loads(request.body)["database_user"]
+        data = json.loads(request.data)["database_user"]
         u = databases.get_user(data["id"])
         if not id or not u:
             abort(404)
@@ -114,26 +111,26 @@ class DatabaseUsersAPI(MethodView):
             return Response(status=204)
         except Exception, e:
             message.complete("error", "%s could not be removed: %s" % (u.name, str(e)))
-            abort(500)
+            raise
 
 
-@backend.route('/db_types/')
+@backend.route('/database_types')
 def list_types():
     if request.args.get("rescan", None):
         databases.scan_managers()
-    return jsonify(database_types=[x.as_dict() for x in databases.get_types()])
+    return jsonify(database_types=[x.as_dict() for x in databases.get_managers()])
 
 
 dbs_view = DatabasesAPI.as_view('dbs_api')
-backend.add_url_rule('/dbs/', defaults={'id': None}, 
+backend.add_url_rule('/databases', defaults={'id': None}, 
     view_func=dbs_view, methods=['GET',])
-backend.add_url_rule('/dbs/', view_func=dbs_view, methods=['POST',])
-backend.add_url_rule('/dbs/<int:id>', view_func=dbs_view, 
+backend.add_url_rule('/databases', view_func=dbs_view, methods=['POST',])
+backend.add_url_rule('/databases/<string:id>', view_func=dbs_view, 
     methods=['GET', 'PUT', 'DELETE'])
 
 dbusers_view = DatabaseUsersAPI.as_view('db_users_api')
-backend.add_url_rule('/db_users/', defaults={'id': None}, 
+backend.add_url_rule('/database_users', defaults={'id': None}, 
     view_func=dbusers_view, methods=['GET',])
-backend.add_url_rule('/db_users/', view_func=dbusers_view, methods=['POST',])
-backend.add_url_rule('/db_users/<int:id>', view_func=dbusers_view, 
+backend.add_url_rule('/database_users', view_func=dbusers_view, methods=['POST',])
+backend.add_url_rule('/database_users/<string:id>', view_func=dbusers_view, 
     methods=['GET', 'PUT', 'DELETE'])
