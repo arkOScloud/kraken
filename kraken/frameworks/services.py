a11952cee6031b09ab91d371eb6cd194ba9e1bd4
Jacob Cook <jacob@peakwinter.net>
2015-02-11 18:08:18 -0500

    services pane

--- kraken/frameworks/services.py
+++ kraken/frameworks/services.py
@@ -1,9 +1,10 @@
 import json
 
-from flask import Response, Blueprint, abort, jsonify
+from flask import Response, Blueprint, abort, jsonify, request
 from flask.views import MethodView
 
 from arkos.system import services
+from kraken.messages import Message
 
 backend = Blueprint("services", __name__)
 
@@ -14,49 +15,59 @@ class ServicesAPI(MethodView):
         if id and not svcs:
             abort(404)
         if type(svcs) == list:
-            return jsonify(services=[x.as_dict() for x in svcs])
+            return jsonify(services=[x.as_dict() for x in svcs if not any(y in x.name for y in ["systemd", "dbus"])])
         else:
             return jsonify(service=svcs.as_dict())
     
     def post(self):
-        data = json.loads(request.body)["service"]
-        svc = services.Service(name=data["name"], cfg=data["cfg"])
+        data = json.loads(request.data)["service"]
+        svc = services.Service(name=data["id"], cfg=data["cfg"])
         svc.add()
-        return jsonify(service=svc)
+        return jsonify(service=svc.as_dict())
     
     def put(self, id):
-        data = json.loads(request.body)["service"]
+        data = json.loads(request.data)["service"]
         svc = services.get(id)
-        if not id or not svc:
+        if id and not svc:
             abort(404)
-        if data.get("operation"):
+        if not data.get("operation"):
+            abort(400)
+        try:
             if data["operation"] == "start":
+                tag = "started"
                 svc.start()
             elif data["operation"] == "stop":
+                tag = "stopped"
                 svc.stop()
             elif data["operation"] == "restart":
+                tag = "restarted"
                 svc.restart()
             elif data["operation"] == "real_restart":
-                svc.real_restart()
+                tag = "restarted"
+                svc.restart(real=True)
             elif data["operation"] == "enable":
+                tag = "enabled"
                 svc.enable()
             elif data["operation"] == "disable":
+                tag = "disabled"
                 svc.disable()
-        else:
-            abort(400)
-        return jsonify(service=svc)
+        except services.ActionError, e:
+            resp = jsonify(message="%s service could not be %s." % (data["id"], tag))
+            resp.status_code = 422
+            return resp
+        return jsonify(service=svc.as_dict())
     
     def delete(self, id):
         svc = services.get(id)
-        if not id or not svc:
+        if id and not svc:
             abort(404)
         svc.remove()
         return Response(status=204)
 
 
 services_view = ServicesAPI.as_view('services_api')
-backend.add_url_rule('/system/services/', defaults={'id': None}, 
+backend.add_url_rule('/system/services', defaults={'id': None}, 
     view_func=services_view, methods=['GET',])
-backend.add_url_rule('/system/services/', view_func=services_view, methods=['POST',])
+backend.add_url_rule('/system/services', view_func=services_view, methods=['POST',])
 backend.add_url_rule('/system/services/<string:id>', view_func=services_view, 
     methods=['GET', 'PUT', 'DELETE'])
