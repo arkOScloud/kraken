9fc2d2bcc0d18a74200fefbc2005e5c0e48e702a
Jacob Cook <jacob@peakwinter.net>
2015-02-26 15:12:19 -0500

    filesystems

--- kraken/frameworks/filesystems.py
+++ kraken/frameworks/filesystems.py
@@ -14,33 +14,29 @@ class DisksAPI(MethodView):
     def get(self, id):
         disks, vdisks = filesystems.get_disk_partitions(), filesystems.get_virtual_disks()
         if id:
-            for x in disks:
-                if x.name == id:
-                    return jsonify(disk_partition=x.as_dict())
-            for x in vdisks:
-                if x.name == id:
-                    return jsonify(virtual_disk=x.as_dict())
+            for x in disks+vdisks:
+                if x.id == id:
+                    return jsonify(filesystem=x.as_dict())
             abort(404)
-        if not disks+vdisks:
+        if id and not disks+vdisks:
             abort(404)
-        return jsonify(disk_partitions=[x.as_dict() for x in disks],
-            virtual_disks=[x.as_dict() for x in vdisks])
+        return jsonify(filesystems=[x.as_dict() for x in disks+vdisks])
     
     def post(self):
-        data = json.loads(request.body)["virtual_disk"]
-        id = as_job(_post, self, data)
-        return job_response(id)
+        data = json.loads(request.data)["filesystem"]
+        id = as_job(self._post, data)
+        return job_response(id, data={"filesystem": data})
     
     def _post(self, data):
         message = Message()
         message.update("info", "Creating virtual disk...")
-        disk = filesystems.VirtualDisk(name=data["name"], size=data["size"])
+        disk = filesystems.VirtualDisk(id=data["id"], size=data["size"])
         try:
             disk.create()
         except Exception, e:
             message.complete("error", "Virtual disk could not be created: %s" % str(e))
             raise
-        if data["encrypt"]:
+        if data["crypt"]:
             try:
                 message.update("info", "Encrypting virtual disk...")
                 disk.encrypt(data["passwd"])
@@ -48,34 +44,36 @@ class DisksAPI(MethodView):
                 disk.remove()
                 message.complete("error", "Virtual disk could not be encrypted: %s" % str(e))
                 raise
-        push_record("virtual_disk", disk.as_dict())
+        message.complete("success", "Virtual disk created successfully")
+        push_record("filesystem", disk.as_dict())
     
     def put(self, id):
-        data = json.loads(request.body)
-        if data.get("virtual_disk"):
-            stype = "virtual_disk"
-            data = data.get("virtual_disk")
-            disk = filesystems.get_virtual_disks(id)
+        data = json.loads(request.data)["filesystem"]
+        disks, vdisks = filesystems.get_disk_partitions(), filesystems.get_virtual_disks()
+        for x in disks+vdisks:
+            if x.id == id:
+                disk = x
+                break
         else:
-            stype = "disk_partition"
-            data = data.get("disk_partition")
-            disk = filesystems.get_disk_partitions(id)
+            abort(404)
         if not id or not disk:
             abort(404)
-        if data["operation"] == "mount":
-            if disk.mountpoint:
-                abort(400)
-            elif disk.crypt and not data.get("passwd"):
-                abort(403)
-            elif data.get("mountpoint"):
-                disk.mountpoint = data["mountpoint"]
-            disk.mount(data.get("passwd"))
-        elif data["operation"] == "umount":
-            disk.umount()
-        if stype == "virtual_disk":
-            return jsonify(virtual_disk=disk.as_dict())
-        else:
-            return jsonify(disk_partition=disk.as_dict())
+        try:
+            if data["operation"] == "mount":
+                if disk.mountpoint:
+                    abort(400)
+                elif disk.crypt and not data.get("passwd"):
+                    abort(403)
+                elif data.get("mountpoint"):
+                    disk.mountpoint = data["mountpoint"]
+                disk.mount(data.get("passwd"))
+            elif data["operation"] == "umount":
+                disk.umount()
+        except Exception, e:
+            resp = jsonify(message="Operation failed: %s" % str(e))
+            resp.status_code = 422
+            return resp
+        return jsonify(filesystem=disk.as_dict(), message="Disk %s successfully"%("mounted" if data["operation"] == "mount" else "unmounted"))
     
     def delete(self, id):
         disk = filesystems.get_virtual_disks(id)
@@ -85,13 +83,13 @@ class DisksAPI(MethodView):
         return Response(status=204)
 
 
-@backend.route('/points/')
+@backend.route('/points')
 def list_points():
     return jsonify(points=[x.as_dict() for x in filesystems.get_points()])
 
 disks_view = DisksAPI.as_view('disks_api')
-backend.add_url_rule('/system/disks/', defaults={'id': None}, 
+backend.add_url_rule('/system/filesystems', defaults={'id': None}, 
     view_func=disks_view, methods=['GET',])
-backend.add_url_rule('/system/disks/', view_func=disks_view, methods=['POST',])
-backend.add_url_rule('/system/disks/<string:id>', view_func=disks_view, 
+backend.add_url_rule('/system/filesystems', view_func=disks_view, methods=['POST',])
+backend.add_url_rule('/system/filesystems/<string:id>', view_func=disks_view, 
     methods=['GET', 'PUT', 'DELETE'])
