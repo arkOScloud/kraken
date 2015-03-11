f7e5abfd9c648ce53b1cbfc7faddefa87749f061
Jacob Cook <jacob@peakwinter.net>
2015-03-11 16:57:30 -0400

    system settings

--- kraken/frameworks/config.py
+++ kraken/frameworks/config.py
@@ -3,7 +3,7 @@ import json
 from flask import Blueprint, jsonify, request
 
 from arkos import config
-from arkos.system import sysconfig
+from arkos.system import sysconfig, systemtime
 
 backend = Blueprint("config", __name__)
 
@@ -18,14 +18,21 @@ def arkos_config():
 @backend.route('/config/hostname', methods=["GET", "PUT"])
 def hostname():
     if request.method == "PUT":
-        sysconfig.set_hostname(json.loads(request.body)["hostname"])
+        sysconfig.set_hostname(json.loads(request.data)["hostname"])
     return jsonify(hostname=sysconfig.get_hostname())
 
 @backend.route('/config/timezone', methods=["GET", "PUT"])
 def timezone():
     if request.method == "PUT":
-        sysconfig.set_timezone(**json.loads(request.body)["timezone"])
+        sysconfig.set_timezone(**json.loads(request.data)["timezone"])
     return jsonify(timezone=sysconfig.get_timezone())
+
+@backend.route('/config/datetime', methods=["GET", "PUT"])
+def datetime():
+    if request.method == "PUT":
+        systemtime.set_datetime()
+    return jsonify(datetime={"date": systemtime.get_date(), 
+        "time": systemtime.get_time(), "offset": systemtime.get_offset()})
 
 @backend.route('/system/shutdown', methods=["POST",])
 def shutdown():
