10e913d18093f89a6a7e49eed2c9356a53d285a8
Jacob Cook <jacob@peakwinter.net>
2015-03-16 18:09:40 -0400

    backup testing

--- kraken/frameworks/backup.py
+++ kraken/frameworks/backup.py
@@ -5,7 +5,7 @@ from flask import Response, Blueprint, abort, jsonify, request
 from flask.views import MethodView
 
 from arkos import backup
-from kraken.messages import Message
+from kraken.messages import Message, push_record
 from kraken.utilities import as_job, job_response
 
 backend = Blueprint("backup", __name__)
@@ -14,44 +14,60 @@ backend = Blueprint("backup", __name__)
 class BackupsAPI(MethodView):
     def get(self, id, time):
         backups = backup.get()
-        if id and id in backups:
-            if time and time in backups[id]:
-                return jsonify(backup=backups[id][time])
-            elif time:
-                abort(404)
-            return jsonify(backups=backups[id])
-        elif id:
+        if id and time and backups:
+            return jsonify(backup=[x for x in backups if x["id"] == id+"/"+time])
+        elif id and backups:
+            return jsonify(backups=[x for x in backups if x["pid"] == id])
+        elif id or time:
             abort(404)
         return jsonify(backups=backups)
     
-    def post(self, id):
-        id = as_job(_post, self, id)
+    def post(self, id, time):
+        id = as_job(self._post, id)
         return job_response(id)
     
     def _post(self, id):
-        backup.create(id)
+        message = Message()
+        message.update("info", "Backing up %s..." % id)
+        try:
+            b = backup.create(id)
+            message.complete("success", "%s backed up successfully" % id)
+            push_record("backups", b)
+        except Exception, e:
+            message.complete("error", "%s could not be backed up: %s" % (id, str(e)))
     
     def put(self, id, time):
-        backups = backup.get()
-        if id in backups and time in backups[id]:
-            id = as_job(_put, self, backups[id][time])
+        data = json.loads(request.data).get("backup")
+        if id and time and data:
+            id = as_job(self._put, data)
             return job_response(id)
         else:
             abort(404)
     
-    def _put(self, backup):
-        backup.restore(backup)
+    def _put(self, data):
+        message = Message()
+        message.update("info", "Restoring %s..." % data["pid"])
+        #try:
+        b = backup.restore(data)
+        message.complete("success", "%s restored successfully" % data["pid"])
+        push_record("backups", b)
+        #except Exception, e:
+        #    message.complete("error", "%s could not be restored: %s" % (data["pid"], str(e)))
     
     def delete(self, id, time):
-        backup.delete(id, time)
+        backup.remove(id, time)
         return Response(status=204)
 
 
+@backend.route('/backups/types')
+def get_possible():
+    return jsonify(types=backup.get_able())
+
+
 backups_view = BackupsAPI.as_view('backups_api')
-backend.add_url_rule('/backups/', defaults={'id': None, 'time': None}, 
+backend.add_url_rule('/backups', defaults={'id': None, 'time': None}, 
     view_func=backups_view, methods=['GET',])
-backend.add_url_rule('/backups/<string:id>', defaults={'time': None}, 
-    view_func=backups_view, methods=['GET',])
-backend.add_url_rule('/backups/<string:id>', view_func=backups_view, methods=['POST',])
-backend.add_url_rule('/backups/<string:id>/<int:time>', view_func=backups_view, 
+backend.add_url_rule('/backups/<string:id>', defaults={'time': None},
+    view_func=backups_view, methods=['GET', 'POST',])
+backend.add_url_rule('/backups/<string:id>/<string:time>', view_func=backups_view, 
     methods=['GET', 'PUT', 'DELETE'])
