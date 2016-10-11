1e0305ff958f225be821b5779531de58dc58965e
Jacob Cook <jacob@peakwinter.net>
2016-10-11 10:56:04 -0400

    0.8.0

--- kraken/framework.py
+++ kraken/framework.py
@@ -21,8 +21,7 @@ def register_frameworks(app):
     """
     fmwkdir = os.path.join(os.path.dirname(__file__), "frameworks")
     for x in os.listdir(fmwkdir):
-        if x.startswith(".") or x == "__init__.py"\
-                or x.endswith((".pyc", ".pyo")):
+        if x.startswith((".", "_")) or x.endswith((".pyc", ".pyo")):
             continue
         x = x.split(".py")[0]
         mod = imp.load_module(x, *imp.find_module(x, [fmwkdir]))
